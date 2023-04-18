2365f41f7907c652fa9f862ea7c7f583c505b958
Arul Murugan Ramalingam <arulmurugan.ramalingam@ideas2it.com>
2023-04-18 16:13:46 +0530

    Changed env values for backend apis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  usersApi: 'a0ab853238197484d980333bba79b154-353187357.ap-south-1.elb.amazonaws.com',
-  tasksApi: 'afb42fde9196649c3a3176dd150cbfa5-1753803018.ap-south-1.elb.amazonaws.com'
+  usersApi: 'ae83b8520f967403ea102c1645a511af-1476041146.us-east-1.elb.amazonaws.com',
+  tasksApi: 'a98e7c80a6c8f41d3afe435ddb53eb8d-1114271319.us-east-1.elb.amazonaws.com'
 };
